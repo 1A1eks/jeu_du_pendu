f1aa78a4734aab5aa413c607dbb3990467216202
Lyioh <bidegaray.lionel@mail.com>
2018-10-04 16:58:40 +0200

    thruday 17:00

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 
 var wonderingWord = prompt("What is the magical word?")
+/*------------------------------- Function underscore ---------------------- */
+/*--------------Putting underscore for each letter -------------------- */
+
+function wonderingWordCaption() {
+        let finalCaption = "";
+    for (i = 0; i <wonderingWord.length; i++) {
+        switch (wonderingWord.charCodeAt[i]) {
+            //if = "-"
+            case 189:
+                let dash = " - ";
+                finalCaption += dash;
+            //
+        }
+        let caption = " _ ";
+        finalCaption += caption;
+    }
+    document.querySelector("#page > #left > h2").innerHTML = finalCaption;
+}
+
+wonderingWordCaption();
 
 const le = wonderingWord.length;
 let check = 0;
@@ -26,13 +46,4 @@ function repro () {
 
 spelling();
 
-function wonderingWordCaption() {
-        let finalCaption = "";
-    for (i = 0; i <wonderingWord.length; i++) {
-        let caption = " _ ";
-        finalCaption += caption;
-    }
-    document.querySelector("#page > #left > h2").innerHTML = finalCaption;
-}
 
-wonderingWordCaption();
