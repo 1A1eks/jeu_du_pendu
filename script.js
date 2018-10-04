f36be8f2e5a3730a376cc3d96f7a87d9ea61ffc3
1A1eks <aleksander.cornette@gmail.com>
2018-10-04 16:59:14 +0200

    mm

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 
 var wonderingWord = prompt("What is the magical word?")
 
-const le = wonderingWord.length;
+
 let check = 0;
 
 wonderingWord = wonderingWord.toUpperCase();
 
+//functions to check spelling
+
 function spelling () {
     for (let i=0; i < le; i++){
         for (let alphab = 0; alphab < 25; alphab++){
-            if (wonderingWord.charCodeAt(i) === "A".charCodeAt(0) + alphab || wonderingWord.charCodeAt(i) === " ".charCodeAt(0)) {
+
+                                                // === 65         + alphab
+
+            if (wonderingWord.charCodeAt(i) === "A".charCodeAt(0) + alphab || wonderingWord.charCodeAt(i) === " ".charCodeAt(0) || 
+            wonderingWord.charCodeAt(i) === "-".charCodeAt(0)) {
                 check++;
             }
         }
     }
-    repro();
+    repro(wonderingWord.length);
 }
 
-function repro () {
+function repro (le) {
     if (le !== check) {
         wonderingWord = prompt("Faulty input! <br> Only use letters or spaces")
+        check = 0;
         spelling();
     }   
 }
 
+//actually using the function!!!
 spelling();
+
+// making underscores!
 
 function wonderingWordCaption() {
         let finalCaption = "";
