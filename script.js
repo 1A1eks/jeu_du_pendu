9d33d75a675d482804c519f1e5eeb0bbcdcfcc03
Lyioh <bidegaray.lionel@mail.com>
2018-10-04 16:25:59 +0200

    img + wonderin word

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-let wonderingWord = prompt("What is the magical word?")
+let wonderingWord = prompt("What is the magical word?");
 
+function wonderingWordCaption() {
+        let finalCaption = "";
+    for (i = 0; i <wonderingWord.length; i++) {
+        let caption = " _ ";
+        finalCaption += caption;
+    }
+    document.querySelector("#page > #left > h2").innerHTML = finalCaption;
+}
+
+wonderingWordCaption();
