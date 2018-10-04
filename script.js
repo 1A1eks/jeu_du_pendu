8c2d79c151a6d630f943db03b0d65cd8a6064a60
1A1eks <aleksander.cornette@gmail.com>
2018-10-04 16:25:54 +0200

    recursive prompt

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-let wonderingWord = prompt("What is the magical word?")
+var wonderingWord = prompt("What is the magical word?")
 
+const le = wonderingWord.length;
+let check = 0;
+
+wonderingWord = wonderingWord.toUpperCase();
+
+function spelling () {
+    for (let i=0; i < le; i++){
+        for (let alphab = 0; alphab < 25; alphab++){
+            if (wonderingWord.charCodeAt(i) === "A".charCodeAt(0) + alphab || wonderingWord.charCodeAt(i) === " ".charCodeAt(0)) {
+                check++;
+            }
+        }
+    }
+    repro();
+}
+
+function repro () {
+    if (le !== check) {
+        wonderingWord = prompt("Faulty input! <br> Only use letters or spaces")
+        spelling();
+    }   
+}
+
+spelling();
