3ec4126fae0e103c51c76ebf0799bcd7e8fd5015
Lyioh <bidegaray.lionel@mail.com>
2018-10-05 12:47:41 +0200

    whitespace

--- script.js
+++ script.js
@@ -4,31 +4,31 @@ var wonderingWord = prompt("What is the magical word?")
 /*--------------Putting underscore for each letter -------------------- */
 
 function wonderingWordCaption() {
-        let finalCaption = "";
-    for (i = 0; i <wonderingWord.length; i++) {
-        switch (wonderingWord.charCodeAt[i]) {
-            //if = "-"
-            case 189:
-                let dash = " - ";
-                finalCaption += dash;
-            //
+    let finalCaption = "";
+    for (i = 0; i < wonderingWord.length; i++) {
+        if (wonderingWord.charCodeAt(i) == "-".charCodeAt(0)) {
+            finalCaption += " -";
+        } else if (wonderingWord.charCodeAt(i) == " ".charCodeAt(0)) {
+            finalCaption += " ";
+        } else {
+            let caption = " _" ;
+            finalCaption += caption;
         }
-        let caption = " _ ";
-        finalCaption += caption;
     }
-    document.querySelector("#page > #left > h2").innerHTML = finalCaption;
+    console.log(finalCaption);
+    document.querySelector("#page > #left > pre").innerText = finalCaption;
 }
 
 wonderingWordCaption();
 
-const le = wonderingWord.length;
+/* const le = wonderingWord.length;
 let check = 0;
 
 wonderingWord = wonderingWord.toUpperCase();
 
-function spelling () {
-    for (let i=0; i < le; i++){
-        for (let alphab = 0; alphab < 25; alphab++){
+function spelling() {
+    for (let i = 0; i < le; i++) {
+        for (let alphab = 0; alphab < 25; alphab++) {
             if (wonderingWord.charCodeAt(i) === "A".charCodeAt(0) + alphab || wonderingWord.charCodeAt(i) === " ".charCodeAt(0)) {
                 check++;
             }
@@ -37,13 +37,13 @@ function spelling () {
     repro();
 }
 
-function repro () {
+function repro() {
     if (le !== check) {
         wonderingWord = prompt("Faulty input! <br> Only use letters or spaces")
         spelling();
-    }   
+    }
 }
 
-spelling();
+spelling(); */
 
 
